2d44228e114e433c945c208360eea32d750eb822
Jesús González <jesus92gz@gmail.com>
2016-07-14 20:03:13 +0200

    Edit output directory for webpack configuration files (both set to dist/ directory)

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,7 +3,7 @@ var path = require('path');
 module.exports = {
   entry: path.join(__dirname, 'src', 'app.js'),
   output: {
-    path: path.join(__dirname, 'dev', 'build'),
+    path: path.join(__dirname, 'build'),
     filename: 'app.js'
   },
   module: {
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var webpack = require('webpack');
 module.exports = {
   entry: path.join(__dirname, 'src', 'app.js'),
   output: {
-    path: path.join(__dirname, 'dist', 'build'),
+    path: path.join(__dirname, 'dist'),
     filename: 'app.js'
   },
   plugins: [
