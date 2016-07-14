64020450dc42f8fb701b7e6c1fdfaab9c64cd338
Jesús González <jesus92gz@gmail.com>
2016-07-14 20:06:03 +0200

    Create About and Cats components

---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About = () => (
+  <section id="about">
+    <h2>Jesús</h2>
+    <p>Doing some coding, hurray!</p>
+  </section>
+);
+
+export default About;
---
+++ src/components/Cats.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Cats = () => (
+  <section id="cats">
+    <p>Cannot you imagine some <strong>funny cats images</strong>?</p>
+  </section>
+);
+
+export default Cats;
