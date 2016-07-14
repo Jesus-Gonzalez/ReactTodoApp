baf1d023189a9f5f48feda011f778ff9b5045e74
Jesús González <jesus92gz@gmail.com>
2016-07-14 20:08:08 +0200

    Create main app entry point

---
+++ src/app.js
@@ -0,0 +1,35 @@
+// Dependencies imports
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+// Redux related stuff
+import todoApp from './reducers/todoApp';
+
+// Presentational components
+import Main from './components/Main.jsx';
+import About from './components/About.jsx';
+import Cats from './components/Cats.jsx';
+
+// Container components
+import TodosList from './components/Todos.jsx';
+
+// Create the store
+const store = createStore(todoApp);
+
+// Development purpose only
+window.store = store;
+
+render(
+        <Provider store={store}>
+          <Router history={hashHistory}>
+            <Route path="/" component={Main}>
+              <IndexRoute component={TodosList} />
+              <Route path="/about" component={About} />
+              <Route path="/funny-cats" components={Cats} />
+            </Route>
+          </Router>
+        </Provider>,
+        document.getElementById('app'));
